2c8763dc2b9059b196e4598b1308a8ea2252f731
Debasish Pradhan <dp0000000004@gmail.com>
2026-06-22 20:29:50 +0530

    Update models.py

--- Restaurant/reservation/models.py
+++ Restaurant/reservation/models.py
@@ -14,7 +14,7 @@ class Reservation(models.Model):
         verbose_name_plural = "رزروها"
 
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.email}"
 
 
         
